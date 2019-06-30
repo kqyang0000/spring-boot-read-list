e98e738ba091cd2b2b42feb8d2e2efa3c8eceed1
kqyang <kqyang0000@163.com>
2019-06-30 11:24:43 +0800

    style code

--- src/main/java/com/imocc/springbootreadlist/SpringBootReadListApplication.java
+++ src/main/java/com/imocc/springbootreadlist/SpringBootReadListApplication.java
@@ -13,8 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootReadListApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootReadListApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReadListApplication.class, args);
+    }
 
 }
